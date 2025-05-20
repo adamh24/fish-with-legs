edf98b5f8ac18efbd24cedd0bc5cc861da204cde
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-05-20 18:16:38 +0100

    modifiers card page

--- src/Modifiers.js
+++ src/Modifiers.js
@@ -1,9 +1,34 @@
+import './Modifiers.css';
+import modifierdata from './data/modifiers.json';
 const Modifiers = () => {
 
 
 
     return (
-        <div>Squirt</div>
+        <div>
+            <div className="title-container">
+                <span className="subtitle">Modifiers</span>
+            </div>
+
+            <div className="modifiers-deck">
+                {modifierdata.map((modifier) => (
+                    <div className="modifier-card">
+                        <div 
+                        className="modifier-image"
+                        style={{
+                            backgroundImage: `url(${require(`${modifier.image}`)})`
+                        }}></div>
+                        <div className="text-block">
+                            <span className="modifiers-title">{modifier.title}</span>
+                            <span className="modifiers-description">{modifier.description}</span>
+                        </div>
+                    </div>
+                ))
+
+                }
+
+            </div>
+        </div>
     );
 }
 
