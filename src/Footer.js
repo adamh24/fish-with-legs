6b6de3fc692020ef01de7759143b4b2ea0edc90e
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-05-21 12:18:58 +0100

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => {
     return (
-        <footer className="footer" >
+        <div className="footer" >
             <div className="footer-content" style={{ textAlign: 'center', padding: '20px 0 0', color: '#fff', marginTop: '10px' }}>
                 <p>© 2025 Fish with Legs Cocktails. All rights reserved.</p>
                 {/* <p>Website by <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">Kitty Sites</a></p> */}
             </div>
-        </footer>
+        </div>
     );
 }
 
