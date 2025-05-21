fcc70ed033c67e5f14f4daf2856d3c87d90377e2
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-05-21 11:52:16 +0100

    get rid of footer background

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="footer" >
-            <div className="footer-content" style={{ textAlign: 'center', padding: '20px 0 0', backgroundColor: '#333', color: '#fff', marginTop: '10px' }}>
+            <div className="footer-content" style={{ textAlign: 'center', padding: '20px 0 0', color: '#fff', marginTop: '10px' }}>
                 <p>© 2025 Fish with Legs Cocktails. All rights reserved.</p>
                 {/* <p>Website by <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">Kitty Sites</a></p> */}
             </div>
