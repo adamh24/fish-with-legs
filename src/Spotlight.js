81a90847a162ff986b297eb9827f53c6bb306ec8
KiranMahn <68765104+KiranMahn@users.noreply.github.com>
2025-05-21 19:59:49 +0100

    fix spotlight

--- src/Spotlight.js
+++ src/Spotlight.js
@@ -3,8 +3,7 @@ const Spotlight = () => {
         <div className="page-container">
         <h1 className="page-title">Spotlight</h1>
         <p>Welcome to the spotlight page!</p>
-        <img src={require("./assets/seaweed.png")} alt="seaweed" style={{ position: 'absolute', bottom: '0px', left: '-100px', height: 'auto', zIndex: 0 }} />
-        <img src={require("./assets/seaweed.png")} alt="seaweed" style={{ position: 'absolute', bottom: '0px', right: '-100px', height: '80vh', zIndex: 0 }} />
+        
         </div>
     );
 }
