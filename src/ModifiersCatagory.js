40caf1b6fb558c5f984545c667730beaca6b9a12
adamh24 <adam.hannay@yahoo.co.uk>
2025-06-08 11:53:48 +0100

    Revert "pain in the a"
    This reverts commit 61008cc1d2e02c1b7eaca5814daa203aef8b1ec8.

--- src/ModifiersCatagory.js
+++ src/ModifiersCatagory.js
@@ -2,7 +2,6 @@
 import modifiers from "./data/modifiers.json";
 import { useParams } from "react-router-dom";
 import "./styles/ModifiersCatagory.css";
-import Allergens from "./components/Allergens.js"
 import { useEffect, useState } from "react";
 
 const ModifiersCatagory = () => {
@@ -18,9 +17,9 @@ const ModifiersCatagory = () => {
 
     const { catagory } = useParams(); // get category title from url 
     console.log("catagory: " + catagory)
-    const subModifiers = modifiers.filter((modifier) => modifier.catagory === catagory);
+    const subModifiers = modifiers.filter((c) => c.catagory === catagory);
     
-    return (
+return (
         <div className={`catagory-page ${isLoaded ? "fade-in" : "fade-out"}`}>
             <div className="title-container">
                 <span className="subtitle">{catagory}</span>
@@ -35,7 +34,9 @@ const ModifiersCatagory = () => {
                             <div className="square-two"></div>
 
                             <div className="icon-group">
-                                        <Allergens modifier={modifier}/>                                  
+                                    <div>
+                                        
+                                    </div>
                             </div>
                             <div className="text-blk">
                                 <span className="modifiers-title">{modifier.title}</span>
