a62b242e35b88bc60036b5c67dfdd3b9ce5d489a
adamha24 <adam.hannay@yahoo.co.uk>
2025-10-19 22:51:17 +0100

    Shadows

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -6,10 +6,10 @@ const Feature = () => {
             <div className='square1'/>
             <div className='square2'/>
             <span className="feature-title">Teaching Fish to Walk</span>
-            <span className="feature-intro">This is a little bit about me and what I do and what you might find on this site.</span>
+            <span className="feature-intro">Step behind the bar and into a world where creativity, knowledge, and sustainability pour together in perfect balance. Whether you’re looking for inspiring cocktail recipes, expert bar techniques, or full training programs to elevate your skills, you’ll find everything you need to shake, stir, and serve with confidence. Great hospitality goes beyond the glass — it’s about craftsmanship, connection, and conscious practice. Explore a community built for bartenders, enthusiasts, and curious minds who care about flavour and the future.</span>
 
         </div>
     );
 }
 
-export default Feature;
+export default Feature;  
